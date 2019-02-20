ebf000786ced09ac56fdeb5bab76dc62c91a0190
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-02-19 19:44:11 -0800

    Enable reset button functionality; minor updates for draws

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,6 +1,7 @@
 
 var counter = 0;
 var marker = 'X';
+var winner = false;
 
 function setMarker(e) {
   //sets a physical marker if the space is empty
@@ -17,12 +18,14 @@ function setMarker(e) {
     document.getElementById(idNumber).innerHTML = marker;
 
     counter++
-    //if it's a draw
-    if (counter === 9) {
-      setTimeout(() => alert("It's a draw! Play again."), 100);
-    }
     //checks the board for a winner combination
     checkForWinner();
+
+    //if it's a draw
+    if (winner === false && counter === 9) {
+      setTimeout(() => alert("It's a draw! Play again."), 50);
+    }
+    console.log(counter)
   }
 }
 
@@ -38,16 +41,28 @@ function checkForWinner() {
   threeInARow(3, 5, 7);
 }
 
-//checks one row for a three-of-a-kind
+//checks one row for a three-of-a-kind, if found, game resets
 function threeInARow (a, b, c) {
   var idValue = document.getElementById(a).innerHTML;
   var idValueTwo = document.getElementById(b).innerHTML;
   var idValueThree = document.getElementById(c).innerHTML;
   if (idValue === 'X' && idValue === idValueTwo && idValueThree === 'X') {
-    setTimeout(() => alert('Player X, winner, winner, chicken dinner!!'), 10);
+    setTimeout(() => alert('Player X, winner, winner, chicken dinner!!'), 50);
+    setTimeout(() => resetGame(), 100);
+    winner = true;
   }
   if (idValue === 'O' && idValue === idValueTwo && idValueThree === 'O') {
-    setTimeout(() => alert('Player O, winner, winner, chicken dinner!!'), 10);
+    setTimeout(() => alert('Player O, winner, winner, chicken dinner!!'), 50);
+    setTimeout(() => resetGame(), 100);
+    winner = true;
   }
 }
 
+function resetGame () {
+  for (var i = 1; i < 10; i++){
+    document.getElementById(i).innerHTML = '';
+  }
+  counter = 0;
+  marker = 'X'
+  winner = false;
+}
