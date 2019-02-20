89a494dc97ccb8f774508e3070bbeddf62ef84ae
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-02-19 18:58:44 -0800

    Clean up code, took out original winner detection code

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -26,8 +26,8 @@ function setMarker(e) {
   }
 }
 
+//checks all possible rows for a three-of-kind
 function checkForWinner() {
-
   threeInARow(1, 2, 3);
   threeInARow(4 ,5, 6);
   threeInARow(7, 8, 9);
@@ -36,106 +36,18 @@ function checkForWinner() {
   threeInARow(3, 6, 9);
   threeInARow(1, 5, 9);
   threeInARow(3, 5, 7);
-
-  // for (var i = 1; i < 2; i++) {
-  //   var idValue = document.getElementById(i).innerHTML;
-  //   var idValueTwo = document.getElementById(i+1).innerHTML;
-  //   var idValueThree = document.getElementById(i+2).innerHTML;
-  //   if (idValue === 'X' && idValue === idValueTwo && idValueThree === 'X') {
-  //     setTimeout(() => alert('Player X, winner, winner, chicken dinner!!'), 100);
-  //   }
-  //   if (idValue === 'O' && idValue === idValueTwo && idValueThree === 'O') {
-  //     setTimeout(() => alert('Player O, winner, winner, chicken dinner!!'), 100);
-  //   }
-  // }
-  // for (var i = 4; i < 5; i++) {
-  //   var idValue = document.getElementById(i).innerHTML;
-  //   var idValueTwo = document.getElementById(i+1).innerHTML;
-  //   var idValueThree = document.getElementById(i+2).innerHTML;
-  //   if (idValue === 'X' && idValue === idValueTwo && idValueThree === 'X') {
-  //     setTimeout(() => alert('Player X, winner, winner, chicken dinner!!'), 100);
-  //   }
-  //   if (idValue === 'O' && idValue === idValueTwo && idValueThree === 'O') {
-  //     setTimeout(() => alert('Player O, winner, winner, chicken dinner!!'), 100);
-  //   }
-  // }
-  // for (var i = 7; i < 8; i++) {
-  //   var idValue = document.getElementById(i).innerHTML;
-  //   var idValueTwo = document.getElementById(i+1).innerHTML;
-  //   var idValueThree = document.getElementById(i+2).innerHTML;
-  //   if (idValue === 'X' && idValue === idValueTwo && idValueThree === 'X') {
-  //     setTimeout(() => alert('Player X, winner, winner, chicken dinner!!'), 100);
-  //   }
-  //   if (idValue === 'O' && idValue === idValueTwo && idValueThree === 'O') {
-  //     setTimeout(() => alert('Player O, winner, winner, chicken dinner!!'), 100);
-  //   }
-  // }
-  // for (var i = 1; i < 2; i += 3) {
-  //   var idValue = document.getElementById(i).innerHTML;
-  //   var idValueTwo = document.getElementById(i+3).innerHTML;
-  //   var idValueThree = document.getElementById(i+6).innerHTML;
-  //   if (idValue === 'X' && idValue === idValueTwo && idValueThree === 'X') {
-  //     setTimeout(() => alert('Player X, winner, winner, chicken dinner!!'), 100);
-  //   }
-  //   if (idValue === 'O' && idValue === idValueTwo && idValueThree === 'O') {
-  //     setTimeout(() => alert('Player O, winner, winner, chicken dinner!!'), 100);
-  //   }
-  // }
-  // for (var i = 2; i < 3; i+= 3) {
-  //   var idValue = document.getElementById(i).innerHTML;
-  //   var idValueTwo = document.getElementById(i+3).innerHTML;
-  //   var idValueThree = document.getElementById(i+6).innerHTML;
-  //   if (idValue === 'X' && idValue === idValueTwo && idValueThree === 'X') {
-  //     setTimeout(() => alert('Player X, winner, winner, chicken dinner!!'), 100);
-  //   }
-  //   if (idValue === 'O' && idValue === idValueTwo && idValueThree === 'O') {
-  //     setTimeout(() => alert('Player O, winner, winner, chicken dinner!!'), 100);
-  //   }
-  // }
-  // for (var i = 3; i < 4; i+= 3) {
-  //   var idValue = document.getElementById(i).innerHTML;
-  //   var idValueTwo = document.getElementById(i+3).innerHTML;
-  //   var idValueThree = document.getElementById(i+6).innerHTML;
-  //   if (idValue === 'X' && idValue === idValueTwo && idValueThree === 'X') {
-  //     setTimeout(() => alert('Player X, winner, winner, chicken dinner!!'), 100);
-  //   }
-  //   if (idValue === 'O' && idValue === idValueTwo && idValueThree === 'O') {
-  //     setTimeout(() => alert('Player O, winner, winner, chicken dinner!!'), 100);
-  //   }
-  // }
-  // for (var i = 1; i < 2; i+= 4) {
-  //   var idValue = document.getElementById(i).innerHTML;
-  //   var idValueTwo = document.getElementById(i+4).innerHTML;
-  //   var idValueThree = document.getElementById(i+8).innerHTML;
-  //   if (idValue === 'X' && idValue === idValueTwo && idValueThree === 'X') {
-  //     setTimeout(() => alert('Player X, winner, winner, chicken dinner!!'), 100);
-  //   }
-  //   if (idValue === 'O' && idValue === idValueTwo && idValueThree === 'O') {
-  //     setTimeout(() => alert('Player O, winner, winner, chicken dinner!!'), 100);
-  //   }
-  // }
-  // for (var i = 3; i < 4; i+= 2) {
-  //   var idValue = document.getElementById(i).innerHTML;
-  //   var idValueTwo = document.getElementById(i+2).innerHTML;
-  //   var idValueThree = document.getElementById(i+4).innerHTML;
-  //   if (idValue === 'X' && idValue === idValueTwo && idValueThree === 'X') {
-  //     setTimeout(() => alert('Player X, winner, winner, chicken dinner!!'), 100);
-  //   }
-  //   if (idValue === 'O' && idValue === idValueTwo && idValueThree === 'O') {
-  //     setTimeout(() => alert('Player O, winner, winner, chicken dinner!!'), 100);
-  //   }
-  // }
 }
 
+//checks one row for a three-of-a-kind
 function threeInARow (a, b, c) {
-    var idValue = document.getElementById(a).innerHTML;
-    var idValueTwo = document.getElementById(b).innerHTML;
-    var idValueThree = document.getElementById(c).innerHTML;
-    if (idValue === 'X' && idValue === idValueTwo && idValueThree === 'X') {
-      setTimeout(() => alert('Player X, winner, winner, chicken dinner!!'), 10);
-    }
-    if (idValue === 'O' && idValue === idValueTwo && idValueThree === 'O') {
-      setTimeout(() => alert('Player O, winner, winner, chicken dinner!!'), 10);
-    }
+  var idValue = document.getElementById(a).innerHTML;
+  var idValueTwo = document.getElementById(b).innerHTML;
+  var idValueThree = document.getElementById(c).innerHTML;
+  if (idValue === 'X' && idValue === idValueTwo && idValueThree === 'X') {
+    setTimeout(() => alert('Player X, winner, winner, chicken dinner!!'), 10);
+  }
+  if (idValue === 'O' && idValue === idValueTwo && idValueThree === 'O') {
+    setTimeout(() => alert('Player O, winner, winner, chicken dinner!!'), 10);
+  }
 }
 
