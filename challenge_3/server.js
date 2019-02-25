1cedc29dae7b19fc6143e2af3b16ca562a5e777c
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-02-24 16:48:13 -0800

    Set up HTML, Express, React and Babel

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,10 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 
+app.listen(8000, () => console.log('listening on port 8000'));
