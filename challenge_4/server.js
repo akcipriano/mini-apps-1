d749bf24ae336e6cc22bca0c0b01ffdea0bd66db
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-02-28 17:24:19 -0800

    Set up Express server, HTML page and webpack/react/babel

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,12 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/client/dist'));
+
+app.listen(8000, console.log('listening on port 8000'));
 
 
